d79aa236a8b46bc139ff406a6d0af85f922b560c
kameshsr <47484458+kameshsr@users.noreply.github.com>
2020-01-24 19:43:48 +0530

    Create Assignment4_Armstrong.java

---
+++ week1/Assignment4_Armstrong.java
@@ -0,0 +1,34 @@
+/*
+Complete the code segment to check whether the number is an Armstrong number or not.
+Mail your queries on studyizfun@gmail.com
+connect me on linkedin
+https://www.linkedin.com/in/kameshShekhar/
+Join Telegram group for more queries
+https://t.me/studyfunk
+Join Whatsapp group for more queries
+https://chat.whatsapp.com/D2C3KlcnskzAywXHmNao3o
+If you like my video 
+Please donate at least RS 10 to my paytm wallet 8809692924
+It will help me financially and encourage me to make more videos
+*/
+import java.util.Scanner;
+public class Exercise1_4 {
+    public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   int n=sc.nextInt();
+           int result=0;
+           int c=0,a,temp; 
+    temp=n;  
+    while(n>0)  
+    {  
+    a=n%10;  
+    n=n/10;  
+    c=c+(a*a*a);  
+    }  
+    if(temp==c)  
+    result=1;   
+    else  
+        result=0;
+System.out.print(result);
+}
+}
