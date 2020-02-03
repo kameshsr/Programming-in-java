200e358fa0e8c94a1af3da44b1e46ecdd010f0cf
kameshsr <47484458+kameshsr@users.noreply.github.com>
2020-02-03 17:57:31 +0530

    Create Assignment5_debug.java

---
+++ week2/Assignment5_debug.java
@@ -0,0 +1,24 @@
+/*
+Complete the code segment to debug the program which is intended to print 'NPTEL JAVA'.
+
+Mail your queries on studyizfun@gmail.com
+connect me on linkedin
+https://www.linkedin.com/in/kameshShekhar/
+Join Telegram group for more queries
+https://t.me/studyfunk
+Join Whatsapp group for more queries
+https://chat.whatsapp.com/D2C3KlcnskzAywXHmNao3o
+If you like my video 
+Please donate at least RS 10 to my paytm wallet 8809692924
+It will help me financially and encourage me to make more videos
+*/
+
+public class Question25{ 
+    public static void main(String[] args) { 
+    
+String nptel=new String("NPTEL");
+String space=new String(" ");
+String java=new String("JAVA");
+ System.out.print(nptel+space+java);
+   }
+}
