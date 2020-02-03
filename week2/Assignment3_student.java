f19ba29946dbed445eff4ad41dc6e9abd9b8eb14
kameshsr <47484458+kameshsr@users.noreply.github.com>
2020-02-03 17:54:04 +0530

    Create Assignment3_student.java

---
+++ week2/Assignment3_student.java
@@ -0,0 +1,31 @@
+/*
+Complete the code segment to call print() method of class Question by creating a method named ‘studentMethod()’.
+Mail your queries on studyizfun@gmail.com
+connect me on linkedin
+https://www.linkedin.com/in/kameshShekhar/
+Join Telegram group for more queries
+https://t.me/studyfunk
+Join Whatsapp group for more queries
+https://chat.whatsapp.com/D2C3KlcnskzAywXHmNao3o
+If you like my video 
+Please donate at least RS 10 to my paytm wallet 8809692924
+It will help me financially and encourage me to make more videos
+*/
+
+public class Question23{ 
+    public static void main(String[] args) { 
+		// Object of the main class is created
+		Question23 q = new Question23();
+		// Print method on object of Question class is called
+		q.studentMethod();
+    }
+	
+	// 'print()' method is defined in class Question
+	void print(Question23 object){
+		System.out.print("Well Done!");
+	}
+void studentMethod(){
+Question23 q=new Question23();
+  q.print(q);
+}
+}
