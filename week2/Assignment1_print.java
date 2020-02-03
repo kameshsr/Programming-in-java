cbe64782c6c620fcc26a6818321848bf6e395f20
kameshsr <47484458+kameshsr@users.noreply.github.com>
2020-02-03 17:49:09 +0530

    Create Assignment1_print.java

---
+++ week2/Assignment1_print.java
@@ -0,0 +1,29 @@
+/*
+Complete the code segment to call the method  print() of class Student.
+
+ Mail your queries on studyizfun@gmail.com
+connect me on linkedin
+https://www.linkedin.com/in/kameshShekhar/
+Join Telegram group for more queries
+https://t.me/studyfunk
+Join Whatsapp group for more queries
+https://chat.whatsapp.com/D2C3KlcnskzAywXHmNao3o
+If you like my video 
+Please donate at least RS 10 to my paytm wallet 8809692924
+It will help me financially and encourage me to make more videos
+*/
+
+class Student { 
+	// This is a method in class Student
+    public void print() { 
+		System.out.println("Hi! I am class STUDENT."); 
+    } 
+}
+
+public class Question21{ 
+    public static void main(String args[]){
+    Student s=new Student();
+s.print();
+
+}
+}
